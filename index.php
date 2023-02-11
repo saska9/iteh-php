2a456e0e87773e61e4710e7af3f399485519f095
Peter67100 <abrankovic862@gmail.com>
2023-02-11 12:21:48 +0100

    napravljena prva verzija početne strane

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Domaći kolači</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans|Candal|Alegreya+Sans">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/imagehover.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/slike.css">
+
+
+  </head>
+  <body>
+    <?php include 'heder.php'; ?>
+
+
+    <section id ="feature" class="section-padding">
+      <div class="container">
+        <div class="row">
+          <div class="header-section text-center">
+
+            <div class="col-md-12 text-center">
+              <img src="img/logo.jpg" class="img img-square"> <br><br>
+            </div>
+            <p>Naš tim je sastavljen od vrhunskih majstora koji se svakodnevno usavršavaju da bismo za Vas stvarali magične slatkiše koji se pamte do kraja života. <br>
+
+Da bismo ispunili sve Vaše slatke maštarije, pratimo najnovije trendove, usavršvamo se i učimo od najboljih svetskih poslastičara.<br>
+
+Na prvom mestu nam je zadovoljstvo, kao i sigurnost naših kupaca pa primenjujemo sistem bezbednosti hrane HACCP. <br>
+
+I zato, torte, kolače pravimo u skladu sa Vašim željama i potrebama, uz stručne savete nasih poslastičara i dekoratera koji će Vam u svakom trenutku objasniti sve što Vas interesuje.<br>
+
+Ukoliko ste ljubitelji domaćih ukusa kolača i torti, poslastičarnica Domaći kolači pravo je mesto za prepuštanje slatkim zadovoljstvima.<br> 
+
+Ko jednom dođe i proba našu slatku ponudu, vraća se po još!<br><br><br>
+
+
+
+ „Domaći kolači“</p>
+<hr class="bottom-line">
+
+  <div class="col-md-12 text-center">
+    <div class="row">
+        <div class="column">
+            <img src="img/pravi.jpg"  style="width:100%">
+        </div>
+        <div class="column">
+            <img src="img/torta.jpg"  style="width:100%">
+        </div>
+        <div class="column">
+            <img src="img/slika.jpg"  style="width:100%">
+        </div>
+        <div class="column">
+            <img src="img/kolac.jpg"  style="width:100%">
+        </div>
+    </div>
+
+  </div>
+
+
+
+
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <?php include 'futer.php'; ?>
+
+
+
+
+
+  </body>
+</html>
